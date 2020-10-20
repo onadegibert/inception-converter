da37e0803238f3151ed3104eff97a35554d411e4
Ona de Gibert <onadegibert@gmail.com>
2020-10-20 16:59:35 +0200

    Fixed format style

--- modules/WebAnnoObject.py
+++ modules/WebAnnoObject.py
@@ -1,5 +1,5 @@
-from modules.TsvFile import TsvFile
-from modules.TsvAnnotation import TsvAnnotation
+from .TsvFile import TsvFile
+from .TsvAnnotation import TsvAnnotation
 
 
 class WebAnnoObject:
@@ -12,10 +12,11 @@ class WebAnnoObject:
     def process_tsv(self):
         final_file = list()
         tsv_file = TsvFile(self.file_object.text, self.file_object.text_string)
-        initial_text = ["#FORMAT=WebAnno TSV 3.2","#T_SP=de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity|identifier|value\n"]
+        initial_text = ["#FORMAT=WebAnno TSV 3.2", "#T_SP=de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity"
+                                                   "|identifier|value\n"]
         final_file.extend(initial_text)
         for each_sen in tsv_file.sentences:
-            final_file.append("\n#Text="+each_sen.string.strip())
+            final_file.append("\n#Text=" + each_sen.string.strip())
             annotated_sentence = TsvAnnotation(each_sen, self.tags, self.file_object.text_string, self.tag_id)
             self.tag_id = annotated_sentence.last_tag_id
             for each_annotation in annotated_sentence.annotation:
