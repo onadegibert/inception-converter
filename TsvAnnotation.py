cc25b9916dd927e3871dd7a914c3715f3fedaa80
onadegibert <onadegibert@gmail.com>
2020-10-20 11:57:36 +0200

    Created annotation class, works when level_2 entity is different

--- TsvAnnotation.py
+++ TsvAnnotation.py
@@ -1,3 +1,6 @@
+import re
+
+
 class TsvAnnotation:
     def __init__(self, sentence, tags, text_string, tag_id):
         self.sentence = sentence
@@ -8,22 +11,39 @@ class TsvAnnotation:
 
     def get_tag(self):
         annotated_sentence = list()
-        last_entity = ""
-        last_annotation = ""
+        last_entity_1 = ""
+        last_entity_2 = ""
+        last_annotation_tag_1 = ""
+        last_annotation_tag_2 = ""
+        last_annotation_entity_1 = ""
+        last_annotation_entity_2 = ""
+        last_annotation_tags = ""
+        last_annotation_entities = ""
         last_tag_id = self.tag_id
         for each_token in self.sentence.tokens:
             annotated_token = AnnotatedToken(each_token, self.sentence, self.tags, self.text_string, self.tag_id)
-            if annotated_token.annotation[4].split("[")[0] == last_entity: # if the last entity is the same
-                if last_entity != "_":
-                    annotated_token.annotation[3] = last_annotation[0]
-                    annotated_token.annotation[4] = last_annotation[1]
-                    last_tag_id = annotated_token.last_tag_id - 2
+            annotation_info = Annotation(re.split(r"\]|\||\[", annotated_token.annotation[4]))
+            if last_entity_1 != "_":
+                if annotation_info.level_1_entity == last_entity_1:  # if the last entity 1 is the same
+                    if annotation_info.level_2_entity == last_entity_2: # if the last entity 2 is the same
+                        annotated_token.annotation[3] = last_annotation_tags
+                        annotated_token.annotation[4] = last_annotation_entities
+                        last_tag_id = annotated_token.last_tag_id - 2
+                    else:
+                        annotated_token.annotation[3] = last_annotation_tag_1 + "|*[" + str(int(annotation_info.level_2_tag) + 1) +  "]"
+                        annotated_token.annotation[4] = last_annotation_entity_1 + "|" + annotation_info.level_2_entity + "[" + str(int(annotation_info.level_2_tag) + 1) + "]"
+                        last_tag_id = int(annotation_info.level_2_tag) + 2
             else:
                 last_tag_id = annotated_token.last_tag_id
             self.tag_id = last_tag_id
-            last_entity = annotated_token.annotation[4].split("[")[0]
-            #TODO compare if the second entity is the same
-            last_annotation = [annotated_token.annotation[3],annotated_token.annotation[4]]
+            last_entity_1 = annotation_info.level_1_entity
+            last_entity_2 = annotation_info.level_2_entity
+            last_annotation_tags = annotated_token.annotation[3]
+            last_annotation_entities = annotated_token.annotation[4]
+            if last_entity_1 != "_":
+                last_annotation_tag_1,last_annotation_tag_2 = annotated_token.annotation[3].split("|")
+                last_annotation_entity_1, last_annotation_entity_2 = annotated_token.annotation[4].split("|")
+            # TODO compare if the second entity is the same
             annotated_sentence.append(annotated_token.annotation)
         return annotated_sentence, last_tag_id
 
@@ -45,7 +65,6 @@ class AnnotatedToken:
         if self.token.string in self.annotated_string and self.token.string != '':
             level_1, level_2 = self.matching_tag()
             level_2_tag_id = self.tag_id + 1
-            # TODO create annotation class
             if level_1 != "_":
                 level_1 = level_1 + "[" + str(self.tag_id) + "]"
                 level_2 = level_2 + "[" + str(level_2_tag_id) + "]"
@@ -73,3 +92,17 @@ class AnnotatedToken:
             elif self.token.onset > tag.onset and self.token.offset < tag.offset:  # in the middle
                 current_tag = tag.level_1, tag.level_2
         return current_tag
+
+
+class Annotation:
+    def __init__(self, annotation_info):
+        if annotation_info[0] != "_":
+            self.level_1_entity = annotation_info[0]
+            self.level_2_entity = annotation_info[3]
+            self.level_1_tag = annotation_info[1]
+            self.level_2_tag = annotation_info[4]
+        else:
+            self.level_1_entity = "_"
+            self.level_2_entity = "_"
+            self.level_1_tag = "_"
+            self.level_2_tag = "_"
