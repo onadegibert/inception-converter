b5f2ed22ad7092c9e27db71c28b7debb999536ba
Ona de Gibert <onadegibert@gmail.com>
2020-11-05 16:39:15 +0100

    Added DATES processing

--- modules/AnnotatedFile.py
+++ modules/AnnotatedFile.py
@@ -1,12 +1,13 @@
 import re
 import os
 
+
 def read_gazetteers():
     # Create a dictionary that stores all gazetteers
     gazetteers = dict()
     for file in os.listdir("gazetteers"):
         filename = file.replace('.txt', '')
-        with open('gazetteers/'+file, 'r') as f:
+        with open('gazetteers/' + file, 'r') as f:
             gazetteers[filename] = f.read().splitlines()
     return gazetteers
 
@@ -52,19 +53,64 @@ class AnnotatedFile:
                 continue
             # Process date tag
             if parsed_tag.level_1 == "DATE":
-                if re.match("^([0-9]{,2}[\/-]){2}[0-9]{2,4}$", parsed_tag.string):
+                # Parse standard abbreviation dates
+                if re.match('([0-9]{,2}[\/-]){2}[0-9]{2,4}$', parsed_tag.string):
                     parsed_tag.level_2 = "standard abbreviation"
                 months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre",
                           "octubre", "noviembre", "diciembre"]
-                if parsed_tag.string.lower() in months:
+                # Parse months
+                if re.search('|'.join(months), parsed_tag.string.lower()):
                     parsed_tag.level_2 = "month"
-                if re.match("^[0-9]{4}$", parsed_tag.string):
+                    # Check if there's days and years
+                    if re.search('[0-9]{2,4}$', parsed_tag.string):
+                        if re.match('[0123][0-9]',parsed_tag.string):
+                            days = re.findall('^[0123][0-9]',parsed_tag.string)
+                            parsed_tag_day_offset = parsed_tag.onset + len(days[0])
+                            parsed_tag_day = Tag(
+                                ['', parsed_tag.level_1, "", parsed_tag.onset, parsed_tag_day_offset, days[0],
+                                 "day"])
+                            parsed_tags.append(parsed_tag_day)
+                            year = re.findall('[0-9]{2,4}', parsed_tag.string)[1]
+                        else:
+                            year = re.findall('[0-9]{2,4}', parsed_tag.string)[0]
+                        months = re.findall('|'.join(months), parsed_tag.string)
+                        stopwords = ["y", "de", "del", "-"]
+                        # TODO Working but level_2 none should be dropped
+                        for month in months:
+                            parsed_tag_month_onset = re.search(month, parsed_tag.string).start() + parsed_tag.onset
+                            parsed_tag_month_offset = parsed_tag_month_onset + len(month)
+                            parsed_tag_month = Tag(
+                                ['', parsed_tag.level_1, "", parsed_tag_month_onset, parsed_tag_month_offset, month,
+                                 "month"])
+                            parsed_tags.append(parsed_tag_month)
+                        if re.search('|'.join(stopwords), parsed_tag.string):
+                            for word in re.findall('|'.join(stopwords), parsed_tag.string):
+                                parsed_tag_word_onset = re.search(word, parsed_tag.string).start() + parsed_tag.onset
+                                parsed_tag.string = parsed_tag.string.replace(word,'xx',1) #replace word to get next occurrence
+                                parsed_tag_word_offset = parsed_tag_word_onset + len(word)
+                                parsed_tag_stopword = Tag(
+                                    ['', parsed_tag.level_1, '', parsed_tag_word_onset, parsed_tag_word_offset, word,
+                                     "none"])
+                                parsed_tags.append(parsed_tag_stopword)
+                        parsed_tag_year_onset = re.search(year, parsed_tag.string).start() + parsed_tag.onset
+                        parsed_tag_year = Tag(
+                            ['', parsed_tag.level_1, "", parsed_tag_year_onset, parsed_tag.offset, year,
+                             "year"])
+                        parsed_tag = parsed_tag_year
+                # Parse years
+                if re.match("^(año )?[0-9]{4}$", parsed_tag.string):
                     parsed_tag.level_2 = "year"
+                    if "año" in parsed_tag.string:
+                        parsed_tag.string = parsed_tag.string.replace("año ", "")
+                        parsed_tag.onset = parsed_tag.onset + len("año") + 1
+
             # process territory tag
             if parsed_tag.level_1 == "ADDRESS" and parsed_tag.level_2 == "territory":
                 if re.match("(?=.*[a-z])(?=.*[A-Z]).*", parsed_tag.string):
                     if parsed_tag.string in gazetteers['spanish_cities']:
                         parsed_tag.level_2 = "city"
+                        if parsed_tag.string in gazetteers['spanish_territories'] and last_tag.level_2 == "city":
+                            parsed_tag.level_2 = "territory"
                 if re.match("^[0-9]*$", parsed_tag.string):
                     parsed_tag.level_2 = "postcode"
             # Process person tag
@@ -78,17 +124,25 @@ class AnnotatedFile:
                     parsed_tags[index] = last_tag
                 # Split one person entity into given and family name
                 elif parsed_tag.level_2 == "other:name" and len(parsed_tag.string.split()) > 1:
-                    given_name = parsed_tag.string.split()[0]
-                    family_name = ' '.join(parsed_tag.string.split()[1:])
+                    if len(parsed_tag.string.split()) > 3:
+                        given_name = ' '.join(parsed_tag.string.split()[:2])
+                        family_name = ' '.join(parsed_tag.string.split()[2:])
+                    else:
+                        given_name = parsed_tag.string.split()[0]
+                        family_name = ' '.join(parsed_tag.string.split()[1:])
 
                     # Process given name entity
                     parsed_tag_given_name_offset = parsed_tag.onset + len(given_name)
-                    parsed_tag_given_name = Tag(['',parsed_tag.level_1, "", parsed_tag.onset, parsed_tag_given_name_offset, given_name, "given name"])
+                    parsed_tag_given_name = Tag(
+                        ['', parsed_tag.level_1, "", parsed_tag.onset, parsed_tag_given_name_offset, given_name,
+                         "given name"])
                     parsed_tags.append(parsed_tag_given_name)
 
                     # Process family name entity
                     parsed_tag_family_name_onset = parsed_tag_given_name.offset + 1
-                    parsed_tag_family_name = Tag(['',parsed_tag.level_1, "", parsed_tag_family_name_onset, parsed_tag.offset, family_name, "family name"])
+                    parsed_tag_family_name = Tag(
+                        ['', parsed_tag.level_1, "", parsed_tag_family_name_onset, parsed_tag.offset, family_name,
+                         "family name"])
                     parsed_tag = parsed_tag_family_name
             parsed_tags.append(parsed_tag)
             last_tag = parsed_tag
--- modules/TsvAnnotation.py
+++ modules/TsvAnnotation.py
@@ -19,13 +19,10 @@ class TsvAnnotation:
         last_annotation_entities = ""
         last_tag_id = self.tag_id
         for each_token in self.sentence.tokens:
-            # TODO process when there are three words as given name - family name
             annotated_token = AnnotatedToken(each_token, self.sentence, self.tags, self.text_string, self.tag_id)
             annotation_info = Annotation(re.split(r"]|\||\[", annotated_token.annotation[4]))
             if last_entity_1 != "_":
-                # TODO Drop none tags at level_1 and level_2
                 if annotation_info.level_1_entity == "none":
-                    print("HELLO")
                     annotated_token.annotation[3] = annotation_info.level_2_tag
                     annotated_token.annotation[4] = annotation_info.level_2_entity
                 elif annotation_info.level_1_entity == last_entity_1:  # if the last entity 1 is the same
@@ -50,9 +47,12 @@ class TsvAnnotation:
             last_annotation_tags = annotated_token.annotation[3]
             last_annotation_entities = annotated_token.annotation[4]
             if last_entity_1 != "_":
-                print(re.split("[\|\n]", '*[10]'))
-                last_annotation_tag_1, last_annotation_tag_2 = re.split("[\|\n]", annotated_token.annotation[3])
-                last_annotation_entity_1, last_annotation_entity_2 = annotated_token.annotation[4].split("|")
+                if len(annotated_token.annotation[3].split("|")) > 1:
+                    last_annotation_tag_1, last_annotation_tag_2 = annotated_token.annotation[3].split("|")
+                    last_annotation_entity_1, last_annotation_entity_2 = annotated_token.annotation[4].split("|")
+                else:
+                    last_annotation_tag_1, last_annotation_tag_2 = annotated_token.annotation[3], ''
+                    last_annotation_entity_1, last_annotation_entity_2 = annotated_token.annotation[4], ''
             annotated_sentence.append(annotated_token.annotation)
         return annotated_sentence, last_tag_id
 
@@ -101,12 +101,19 @@ class TsvAnnotation:
                         else:
                             last_tag_id = annotated_token.last_tag_id - 2
                     else:
-                        annotated_token.annotation[3] = last_annotation_tag_1 + "|*[" + str(
-                            annotated_token.last_tag_id - 2) + "]"
-                        annotated_token.annotation[
-                            4] = last_annotation_entity_1 + "|" + annotation_info.level_2_entity + "[" + str(
-                            annotated_token.last_tag_id - 2) + "]"
-                        last_tag_id = (annotated_token.last_tag_id - 1)
+                        if annotation_info.level_2_entity == "none": # Cases of stopwords within dates such as "5 de Noviembre de 2020"
+                            annotated_token.annotation[3] = "*[" + str(int(annotation_info.level_1_tag) - 2) + "]"
+                            annotated_token.annotation[4] = annotation_info.level_1_entity + annotated_token.annotation[
+                                3].replace("*", "")
+                            last_tag_id = int(annotation_info.level_1_tag)
+                            annotated_token.last_tag_id = last_tag_id
+                        else:
+                            annotated_token.annotation[3] = last_annotation_tag_1 + "|*[" + str(
+                                annotated_token.last_tag_id - 2) + "]"
+                            annotated_token.annotation[
+                                4] = last_annotation_entity_1 + "|" + annotation_info.level_2_entity + "[" + str(
+                                annotated_token.last_tag_id - 2) + "]"
+                            last_tag_id = (annotated_token.last_tag_id - 1)
                 else:
                     last_tag_id = annotated_token.last_tag_id
             else:
