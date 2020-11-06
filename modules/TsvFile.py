af6f913096e038f200b038b2a2e5f317c074747d
Ona de Gibert <onadegibert@gmail.com>
2020-11-06 11:57:30 +0100

    Only process non-empty lines

--- modules/TsvFile.py
+++ modules/TsvFile.py
@@ -14,10 +14,11 @@ class TsvFile:
         onset_start = 0
         # TODO improve sentence splitting
         for sentence in orig_sentences:
-            # re.split(':|,| |\.(?!com)'
-            sentence = Sentence(sentence, orig_sentences.index(sentence),
-                                re.findall(r"[\w']+|[!\"\[\]#$%&'()*+,-./:;<=>?@^_`{|}~]", sentence), self.text_string,
-                                onset_start)
-            onset_start = sentence.onset_start
-            sentences.append(sentence)
+            if sentence != '\n':
+                # re.split(':|,| |\.(?!com)'
+                sentence = Sentence(sentence, orig_sentences.index(sentence),
+                                    re.findall(r"[\w']+|[!\"\[\]#$%&'()*+,-./:;<=>?@^_`{|}~]", sentence), self.text_string,
+                                    onset_start)
+                onset_start = sentence.onset_start
+                sentences.append(sentence)
         return sentences
