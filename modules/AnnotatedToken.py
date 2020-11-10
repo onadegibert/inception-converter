9f8014c1b853d25ee8fcf5d031fef4c3c2377b31
Ona de Gibert <onadegibert@gmail.com>
2020-11-10 15:28:05 +0100

    Only correct other:name tags when Nombre and Apellidos appear

--- modules/AnnotatedToken.py
+++ modules/AnnotatedToken.py
@@ -44,8 +44,9 @@ class AnnotatedToken:
             elif self.token.onset > tag.onset and self.token.offset < tag.offset:  # in the middle
                 current_tag = tag.level_1, tag.level_2
         # Process Nombre y Apellidos for other:name tags
-        if re.search("Nombre", self.sentence.string):
-            current_tag = current_tag[0], "given name"
-        elif re.search("Apellidos", self.sentence.string):
-            current_tag = current_tag[0], "family name"
+        if current_tag[1] == "other:name":
+            if re.search("Nombre", self.sentence.string):
+                current_tag = current_tag[0], "given name"
+            elif re.search("Apellidos", self.sentence.string):
+                current_tag = current_tag[0], "family name"
         return current_tag
